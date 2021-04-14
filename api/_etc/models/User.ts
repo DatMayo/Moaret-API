1eaf7c876df9f05cb8e8db6545be552e8ee8dfa0
Mario Franze <mario.franze@gmail.com>
2021-04-14 20:28:16 +0200

    Added Display Name & Timestamp

--- api/_etc/models/User.ts
+++ api/_etc/models/User.ts
@@ -1,18 +1,24 @@
 import * as mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Please provide a username"],
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please provide a username"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+    locked: {
+      type: Number,
+      default: 0,
+    },
+    displayName: {
+      type: String,
+    },
   },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-  },
-  locked: {
-    type: Number,
-    default: 0,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.UserSchema || mongoose.model("User", UserSchema);
