33a5209467748d615f9ee7a8e6c2c049a13b03cb
Mario Franze <mfranze@codingden.de>
2021-12-04 16:03:54 +0100

    Added try catch

--- api/_etc/logic/Token.ts
+++ api/_etc/logic/Token.ts
@@ -1,29 +1,34 @@
-import { IError, IResponse } from "../interfaces";
+import { IResponse } from "../interfaces";
 import TokenSchema from "../../_etc/models/Token";
 
 export class Token {
-  private _errors: IError[] = [];
-
   async create(guid: string): Promise<IResponse> {
-    const tokenHandle = await TokenSchema.findOne({
-      userId: guid,
-    });
-    if (tokenHandle) {
-      tokenHandle.updatedAt = new Date();
-      await tokenHandle.save();
-      return {
-        code: 200,
-        data: { tokenInfo: tokenHandle },
-      };
-    } else {
-      const currentToken = Math.random().toString(36).substring(7);
-      const tokenResult = await new TokenSchema({
-        token: currentToken,
+    try {
+      const tokenHandle = await TokenSchema.findOne({
         userId: guid,
-      }).save();
+      });
+      if (tokenHandle) {
+        tokenHandle.updatedAt = new Date();
+        await tokenHandle.save();
+        return {
+          code: 200,
+          data: { tokenInfo: tokenHandle },
+        };
+      } else {
+        const currentToken = Math.random().toString(36).substring(7); // 5874311
+        const tokenResult = await new TokenSchema({
+          token: currentToken,
+          userId: guid,
+        }).save();
+        return {
+          code: 200,
+          data: { tokenInfo: tokenResult },
+        };
+      }
+    } catch (err) {
       return {
-        code: 200,
-        data: { tokenInfo: tokenResult },
+        code: 500,
+        error: [{ msg: err }],
       };
     }
   }
